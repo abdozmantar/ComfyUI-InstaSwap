8c862bbecc3783bc4d5e2b771228d519e36eb472
Abdullah Özmantar <157515815+abdozmantar@users.noreply.github.com>
2024-06-19 15:51:22 +0300

    Update instaswap_version.py

--- scripts/instaswap_version.py
+++ scripts/instaswap_version.py
@@ -11,4 +11,5 @@ app_title = "InstaSwap Face Swapper Node for ComfyUI"
 version_flag = "v1.0"
 
 from scripts.instaswap_logger import logger
+
 logger.info(f"InstaSwap Face Swapper node version {version_flag} running in ComfyUI")
