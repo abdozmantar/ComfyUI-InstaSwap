9da301158d7d9d5211ece98f2a3f0a626a8a3473
Abdullah Özmantar <157515815+abdozmantar@users.noreply.github.com>
2024-06-20 16:43:33 +0300

    Update instaswap_swapper.py

--- scripts/instaswap_swapper.py
+++ scripts/instaswap_swapper.py
@@ -10,10 +10,12 @@
 import copy
 import os
 import shutil
+
 from dataclasses import dataclass
 from typing import List, Union
 import cv2
 import numpy as np
+
 from PIL import Image
 import insightface
 from insightface.app.common import Face
